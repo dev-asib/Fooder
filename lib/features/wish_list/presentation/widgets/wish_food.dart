c85a605b09ba7e7d763af879bda4ea2d19689e60
dev-asib <mail.asib.com@gmail.com>
2024-12-14 22:57:27 +0600

    Bug Fix

--- lib/features/wish_list/presentation/widgets/wish_food.dart
+++ lib/features/wish_list/presentation/widgets/wish_food.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:fooder/core/exported_files/exported_files.dart';
-import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 
 class WishFood extends StatelessWidget {
@@ -20,57 +19,54 @@ class WishFood extends StatelessWidget {
       onTap: () => _onTapWishFood(context),
       child: Card(
         elevation: 3,
-        child: FittedBox(
-          child: Stack(
-            alignment: Alignment.topRight,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Food(imgPath: food.imgPath!),
-                    Text(
-                      food.foodTitle!,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: textTheme.bodyLarge,
-                    ),
-                    Text(
-                      food.foodDetails!,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: textTheme.bodyMedium,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "TK ${wishListProvider.totalSubFoodPrice(food)}",
-                          style: textTheme.bodyLarge,
-                        ),
-                        const Gap(8),
-                        HorizontalIconIconWithText(
-                          title: "${food.reviewList![0].rating}",
-                          icon: Icons.star,
-                          middleGap: 1,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Food(imgPath: food.imgPath!),
+                  Text(
+                    food.foodTitle!,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.bodyLarge,
+                  ),
+                  Text(
+                    food.foodDetails!,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.bodyMedium,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "TK ${wishListProvider.totalSubFoodPrice(food)}",
+                        style: textTheme.bodyLarge,
+                      ),
+                      HorizontalIconIconWithText(
+                        title: "${food.reviewList![0].rating}",
+                        icon: Icons.star,
+                        middleGap: 1,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Positioned(
-                top: 8,
-                right: 8,
-                child: SizedBox(
-                  height: 30,
-                  width: 30,
-                  child: WishIcon(food: food),
-                ),
+            ),
+            Positioned(
+              top: 8,
+              right: 8,
+              child: SizedBox(
+                height: 30,
+                width: 30,
+                child: WishIcon(food: food),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
